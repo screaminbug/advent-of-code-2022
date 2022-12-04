f0b0e69e1c6c645cc7e35ab60706d77a2208b39b
Tomislav <tstrelar@gmail.com>
2022-12-04 14:05:22 +0100

    Solve day 4

--- src/main/kotlin/hr/tstrelar/advent_of_code_2022/application/service/DayFourService.kt
+++ src/main/kotlin/hr/tstrelar/advent_of_code_2022/application/service/DayFourService.kt
@@ -1,4 +1,36 @@
 package hr.tstrelar.advent_of_code_2022.application.service
 
-class DayFourService {
+import hr.tstrelar.NL
+import hr.tstrelar.advent_of_code_2022.application.port.`in`.FullyOverlappingAssignmentsUseCase
+import hr.tstrelar.advent_of_code_2022.application.port.`in`.OverlappingAssignmentsUseCase
+
+class DayFourService : FullyOverlappingAssignmentsUseCase, OverlappingAssignmentsUseCase {
+    override fun howManyFullyOverlap(input: String) = input.split(NL).map {
+        val a = extractAssignments(it)
+        if ((a.elf1Start <= a.elf2Start && a.elf1End >= a.elf2End) || (a.elf1Start >= a.elf2Start && a.elf1End <= a.elf2End))
+            1 else 0
+    }.sum().toString()
+    override fun howManyOverlap(input: String) = input.split(NL).map {
+        val a = extractAssignments(it)
+        val elf1 = (a.elf1Start .. a.elf1End)
+        val elf2 = (a.elf2Start .. a.elf2End)
+        if (elf1.intersect(elf2).isNotEmpty()) 1 else 0
+    }.sum().toString()
+    private fun extractAssignments(input: String): Assignments {
+        val assignments = input.split(",")
+        val firstAssignmentSections = assignments[0].split("-")
+        val secondAssignmentSections = assignments[1].split("-")
+        return Assignments(
+            elf1Start = firstAssignmentSections[0].toInt(),
+            elf1End = firstAssignmentSections[1].toInt(),
+            elf2Start = secondAssignmentSections[0].toInt(),
+            elf2End = secondAssignmentSections[1].toInt()
+        )
+    }
+    data class Assignments(
+        val elf1Start: Int,
+        val elf1End: Int,
+        val elf2Start: Int,
+        val elf2End: Int
+    )
 }
--- src/test/kotlin/hr/tstrelar/unit/Day4Test.kt
+++ src/test/kotlin/hr/tstrelar/unit/Day4Test.kt
@@ -1,16 +1,28 @@
 package hr.tstrelar.unit
 
+import hr.tstrelar.advent_of_code_2022.application.port.`in`.FullyOverlappingAssignmentsUseCase
+import hr.tstrelar.advent_of_code_2022.application.port.`in`.OverlappingAssignmentsUseCase
 import hr.tstrelar.advent_of_code_2022.application.service.DayFourService
 import org.junit.jupiter.api.Test
 
 internal class Day4Test {
     private val dayFourService = DayFourService()
+    private val fullyOverlappingAssignmentsUseCase: FullyOverlappingAssignmentsUseCase = dayFourService
+    private val overlappingAssignmentsUseCase: OverlappingAssignmentsUseCase = dayFourService
 
     @Test
-    fun sumOfPriorities() {
-    }
+    fun sumOfPriorities() = runTestFor(
+        day = 4,
+        part = 1,
+        fullyOverlappingAssignmentsUseCase::howManyFullyOverlap,
+        "2"
+    )
 
     @Test
-    fun sumOfBadgePriorities() {
-    }
+    fun sumOfBadgePriorities() = runTestFor(
+        day = 4,
+        part = 2,
+        overlappingAssignmentsUseCase::howManyOverlap,
+        expected = "4"
+    )
 }
